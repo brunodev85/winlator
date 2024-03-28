8f79ebcd31e0b426fadb4502338b8e681fb21c13
brunodev85 <korg_bruno@yahoo.com.br>
2024-03-28 09:47:01 -0300

    Add workaround for Daggerfall Unity

--- app/src/main/java/com/winlator/core/Workarounds.java
+++ app/src/main/java/com/winlator/core/Workarounds.java
@@ -10,11 +10,13 @@ public abstract class Workarounds {
     // Workaround for applications that don't work mouse/keyboard
     public static void onMapWindow(final WinHandler winHandler, Window window) {
         final String className = window.getClassName();
-        if (className.equals("twfc.exe")) {
+        if (className.equals("twfc.exe") ||
+            className.equals("daggerfallunity.exe")) {
+            final boolean reverse = className.equals("twfc.exe");
             (new Timer()).schedule(new TimerTask() {
                 @Override
                 public void run() {
-                    winHandler.bringToFront(className, true);
+                    winHandler.bringToFront(className, reverse);
                 }
             }, 500);
         }
